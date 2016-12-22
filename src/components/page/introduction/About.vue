ddfc7fecb4c71a77d3f891553875c161095b376c
corenel <xxdsox@gmail.com>
2016-12-22 22:01:17 +0800

    Modify sidebar

---
+++ src/components/page/introduction/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>愚蠢的人类</h1>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<style>
+</style>
+<script>
+    export default{
+        data () {
+            return {
+                msg: 'hello vue'
+            }
+        },
+        components: {
+        }
+    }
+</script>
